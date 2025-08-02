1d27647d743c994a9e89f04b4cea50d9ad37b803
qkzk <qu3nt1n@gmail.com>
2025-08-02 20:28:03 +0200

    documentation

--- lua/fm_picker/init.lua
+++ lua/fm_picker/init.lua
@@ -1,18 +1,19 @@
 local M = {}
 
 local config = {
+	-- where is fm executable on your system ? `$ which fm` should tell you !
 	fm_path = "/usr/bin/fm",
 }
 
 function M.setup(user_config)
+	--- update the config
 	config = vim.tbl_deep_extend("force", config, user_config or {})
 end
 
+---Entry point. Toggle the fm file picker.
 function M.toggle()
-	local fm_exec = vim.fn.fnameescape(config.fm_path)
-	local Toggleterm = require("toggleterm.terminal").Terminal
-
-	require("fm_picker.internal").toggle_with_path(fm_exec)
+	local fm_path = vim.fn.fnameescape(config.fm_path)
+	require("fm_picker.internal").toggle_with_path(fm_path)
 end
 
 return M
--- lua/fm_picker/internal.lua
+++ lua/fm_picker/internal.lua
@@ -3,7 +3,8 @@ local fm_term
 local Toggleterm = require("toggleterm.terminal").Terminal
 local selected_filepath = nil
 
--- Close all opened buffers with this filepath
+---Close all opened buffers with this filepath
+---@param path string filename of the buffer which should be deleted
 local function delete_buffer_by_path(path)
 	local bufs = vim.api.nvim_list_bufs()
 	for _, buf in ipairs(bufs) do
@@ -16,7 +17,8 @@ local function delete_buffer_by_path(path)
 	end
 end
 
--- Open path in buffer and toggle fm
+---Open path in buffer and toggle fm
+---@param filepath string filename of the picked file which should be opened in neovim
 local function open_buffer_by_path(filepath)
 	if fm_term then
 		selected_filepath = filepath
@@ -25,7 +27,8 @@ local function open_buffer_by_path(filepath)
 	vim.cmd("edit " .. vim.fn.fnameescape(filepath))
 end
 
--- Parse and execute messages.
+---Parse and execute messages.
+---@param data string received message through receiving socket.
 local function handle_data(data)
 	local msg = vim.trim(data)
 	if vim.startswith(msg, "OPEN ") then
@@ -41,10 +44,13 @@ local function handle_data(data)
 	end
 end
 
--- Handle a received message.
--- Log errors to neovim logs,
--- Execute commands,
--- Close the connection if EOF is reached.
+---Handle a received message.
+---Log errors to neovim logs,
+---Execute commands,
+---Close the connection if EOF is reached.
+---@param err error_types error received while receiving data
+---@param data string | nil message received
+---@param client userdata socket client
 local function handle_message(err, data, client)
 	if err then
 		vim.notify("Error reading socket reply fm: " .. err, vim.log.levels.ERROR)
@@ -59,7 +65,9 @@ local function handle_message(err, data, client)
 	end
 end
 
--- Start a server listening on `fm_nvim_socket_path` and awaiting IPC commands.
+---Start a server listening on `fm_nvim_socket_path` and awaiting IPC commands.
+---Handle the received commands and close the connection if need be.
+---@param fm_nvim_socket_path string filepath to the UNIX socket file used by fm to send msg to neovim
 local function start_reply_socket(fm_nvim_socket_path)
 	local server = vim.loop.new_pipe(false)
 	local ok, err = pcall(function()
@@ -85,7 +93,11 @@ local function start_reply_socket(fm_nvim_socket_path)
 	end)
 end
 
--- Send a message through UNIX Socket
+---Send a message through UNIX Socket.
+---NB. Since the only kind of message we want to send is "go", there's no message parameter.
+---It may change in the future if we decide to implement more possible actions.
+---fm-tui allows more message like "KEY <key>" or "ACTION <action>" which may take full control of the process.
+---@param nvim_fm_socket_path string filepath to the UNIX socket file used by neovim to send msg to fm
 local function send_to_fm_socket(nvim_fm_socket_path, file_path)
 	local sock = vim.loop.new_pipe(false)
 	vim.loop.pipe_connect(sock, nvim_fm_socket_path, function(err)
@@ -101,8 +113,14 @@ local function send_to_fm_socket(nvim_fm_socket_path, file_path)
 	end)
 end
 
--- Creates the fm command with all required parameters
--- Requires fm_tui version >=0.2.1
+---Creates the fm _command_ with all required parameters.
+---Doens't run the command itself.
+---Requires fm_tui version >=0.2.1
+---@param fm_path string filepath to fm executable
+---@param servername string servername opened by neovim. Won't be use and may be removed once the API stabilized.
+---@param file_path string filepath of the current buffer which will be selected.
+---@param nvim_fm_socket_path string filepath to the socket used to send message from nvim to fm.
+---@param fm_nvim_socket_path string filepath to the socket used to send message from fm to nvim.
 local function build_fm_command(fm_path, servername, file_path, nvim_fm_socket_path, fm_nvim_socket_path)
 	return fm_path
 		.. " --neovim -l -s "
@@ -115,7 +133,9 @@ local function build_fm_command(fm_path, servername, file_path, nvim_fm_socket_p
 		.. fm_nvim_socket_path
 end
 
--- Toggle the FM file picker
+---Toggle the FM file picker.
+---Entry point of the plugin.
+---@param fm_path string filepath to the fm executable.
 function M.toggle_with_path(fm_path)
 	-- socket used to send messages from fm to nvim
 	local nvim_fm_socket_path = "/tmp/nvim-fm-" .. tostring(vim.fn.getpid()) .. ".sock"
